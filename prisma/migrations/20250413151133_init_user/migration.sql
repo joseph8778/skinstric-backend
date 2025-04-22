15afb0565e95afc49e9fae7be0b7ffeeaf6c1cae
Joseph <joseph8778@gmail.com>
2025-04-22 10:23:53 -0500

    Initial commit

---
+++ prisma/migrations/20250413151133_init_user/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
---
+++ server.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import { configDotenv } from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+
+
+app.get('/api/ping', (req, res) => {
+    console.log('Frontend just pinged me!');
+    res.json({message: 'pong from backend!'})
+})
+const prisma = new PrismaClient();
+app.post('/api/user', async (req, res) => {
+  const { clerkUserId, email, preferredName, formattedAddress, latitude, longitude, demoData } = req.body;
+
+  if (!clerkUserId || !email) {
+    return res.status(400).json({ message: 'Missing clerkUserId or email' });
+  }
+
+  try {
+    const user = await prisma.user.upsert({
+      where: { clerkUserId },
+      update: {
+        email,
+        preferredName,
+        formattedAddress,
+        latitude,
+        longitude,
+        demoData
+      },
+      create: {
+        clerkUserId,
+        email,
+        preferredName,
+        formattedAddress,
+        latitude,
+        longitude,
+        demoData
+      },
+    });
+
+    res.status(200).json({ message: 'User synced', user });
+    console.log('User info synced on backend')
+  } catch (err) {
+    console.error('❌ DB error:', err);
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
+app.get('/api/user/:clerkUserId', async (req, res) => {
+  const { clerkUserId } = req.params;
+  try {
+    const userData = await prisma.user.findUnique({
+      where: { clerkUserId }
+    })
+    
+    if( !userData ) {
+        return res.status(404).json({message: 'User not found'})
+    } 
+    
+    res.status(200).json({user: userData})
+  } catch(err) {
+    console.log('Error fetching userData:', err);
+    res.status(500).json({message: 'Server error', error})
+  }
+})
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
