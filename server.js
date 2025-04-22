44b1580003264087e6f0e276ef11417e9a23caf4
Joseph <joseph8778@gmail.com>
2025-04-22 13:21:41 -0500

    changed origin url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: "https://skinstric-jt-rdpp.vercel.app" }));
 app.use(express.json());
 
 
